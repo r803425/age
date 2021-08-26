5dc0d4571ad40ca9c80672358827987b15989f25
Ryan <r803425@gmail.com>
2021-08-27 03:50:52 +0800

    finished2 age.py

--- age.py
+++ age.py
@@ -1,8 +1,8 @@
 driving = input('請問你有沒有開過車?')
 if driving != '有' and driving != '沒有':
     print('只能輸入 有/沒有')
-    raise SystemExit
-    
+    raise SystemExit #觸發系統錯誤離開
+
 age = input('請問你的年齡?')
 age = int(age)
 if driving == '有':
